0da3c3432562e2d749a58aa5d4a09ea4dfc71352
kushal <kushalsaini716@gmail.com>
2026-06-25 23:42:00 +0530

    Fix Login filename case

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "./services/api";
 
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
